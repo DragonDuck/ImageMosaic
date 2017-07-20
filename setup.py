9eb15161d87701cc20076f9739faad07a33fb2c1
Jan Sauer <jpsauer@jan-sauer.de>
2017-07-20 22:24:27 +0200

    Bugfix

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='ImageMosaic',
     version='1.0',
     packages=['ImageMosaic',],
-    license='GNU General Public License Version 3'
-    long_description=open('README.md').read(),
+    license='GNU General Public License Version 3',
+    long_description=open('README.md').read()
 )
